10ed58a42e115d62d5c3e65fe7d250e9f78e950f
Blackhacker-a <64885014+Blackhacker-a@users.noreply.github.com>
2020-05-13 00:12:38 +0700

    Em sửa code 1 tẹo(tại nếu đúng ra thì bài này in ra nhiều số lắm)

--- Bai 2.cpp
+++ Bai 2.cpp
@@ -3,8 +3,9 @@ int main(){
 	int n;
 	printf("Nhap vao so nguyen duong n ");
 	scanf("%d",&n);
-	printf("Cac so chia het cho 3 nho hon %d la\n",n);
-	for(int i=-n;i<n;i++){
+	printf("Cac so nguyen duong chia het cho 3 nho hon %d la\n",n);
+	// hoi nhu de luc dau thi se co vo han so vi co rat nhieu so am nho hon n
+	for(int i=0;i<n;i++){
 	    if(i%3==0){
 		printf("%2d\t",i);
 		}	
